4422127279058fb6cf44aed8627efa79a3be4ee5
Aleksey Sidorov <alekseysidorov.28@gmail.com>
2022-04-06 20:40:25 +0300

    Сделал русификацию, но нужно сделать правки с названиями Breakfast, Snack и т.д., т.к. редьюсеры завязаны на  этих названиях

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,21 +1,21 @@
 const initialState = {
-    Breakfast: [],
-    Lunch: [],
-    Dinner: [],
-    Snack: []
+	Breakfast: [],
+	Lunch: [],
+	Dinner: [],
+	Snack: [],
 };
 
 const reducer = (state = initialState, { type, payload }) => {
-    switch (type) {
-        case 'ADD_DISH':
-            return {...state, [payload.mealName]: [...state[payload.mealName], payload] };
-        case 'DELETE_DISH':
-            return {...state, [payload.mealName]: state[payload.mealName].filter((dish) => dish.id !== payload.id) };
-        case 'CHANGE_COUNT_DISH':
-            return {...state, [payload.mealName]: [...payload.data] };
-        default:
-            return state;
-    }
+	switch (type) {
+		case 'ADD_DISH':
+			return { ...state, [payload.mealName]: [...state[payload.mealName], payload] };
+		case 'DELETE_DISH':
+			return { ...state, [payload.mealName]: state[payload.mealName].filter((dish) => dish.id !== payload.id) };
+		case 'CHANGE_COUNT_DISH':
+			return { ...state, [payload.mealName]: [...payload.data] };
+		default:
+			return state;
+	}
 };
 
 export default reducer;
