6e7988fb5f198ea6aca7fb31ffe547e561757ffe
Alexandr <alexryapolov36@gmail.com>
2022-04-10 20:37:40 +0400

    add product to server

--- src/constants.js
+++ src/constants.js
@@ -1,37 +1,47 @@
 import { v4 as uuid4 } from 'uuid';
 
-export const daysOfWeek = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+export const daysOfWeek = [
+  'Воскресенье',
+  'Понедельник',
+  'Вторник',
+  'Среда',
+  'Четверг',
+  'Пятница',
+  'Суббота',
+];
 
 export const REQUEST_STATUS = {
-	CREATED: 0,
-	LOADING: 1,
-	SUCCESS: 2,
-	FAILURE: 3,
+  CREATED: 0,
+  LOADING: 1,
+  SUCCESS: 2,
+  FAILURE: 3,
 };
 
 export const MEALS = [
-	{
-		id: uuid4(),
-		title: 'breakfast',
-		titleRus: 'Завтрак',
-		expanded: true,
-	},
-	{
-		id: uuid4(),
-		title: 'lunch',
-		titleRus: 'Обед',
-		expanded: false,
-	},
-	{
-		id: uuid4(),
-		title: 'dinner',
-		titleRus: 'Ужин',
-		expanded: false,
-	},
-	{
-		id: uuid4(),
-		title: 'snack',
-		titleRus: 'Перекус',
-		expanded: false,
-	},
+  {
+    id: uuid4(),
+    title: 'breakfast',
+    titleRus: 'Завтрак',
+    expanded: true,
+  },
+  {
+    id: uuid4(),
+    title: 'lunch',
+    titleRus: 'Обед',
+    expanded: false,
+  },
+  {
+    id: uuid4(),
+    title: 'dinner',
+    titleRus: 'Ужин',
+    expanded: false,
+  },
+  {
+    id: uuid4(),
+    title: 'snack',
+    titleRus: 'Перекус',
+    expanded: false,
+  },
 ];
+
+export const API = 'http://213.226.114.162/api/';
--- src/store/Dishes/actions.js
+++ src/store/Dishes/actions.js
@@ -1,35 +1,71 @@
+import { API } from '../../constants';
 export const REQUEST_DISHES_LOADING = 'DISHES::REQUEST_DISHES_LOADING';
 export const REQUEST_DISHES_FILURE = 'DISHES::REQUEST_DISHES_FAILURE';
 export const REQUEST_DISHES_SUCCESS = 'DISHES::REQUEST_DISHES_SUCCESS';
+export const REQUEST_ADD_DISH = 'DISHES::REQUEST_ADD_DISH';
+export const REQUEST_ADD_DISH_SUCCESS = 'DISHES::REQUEST_ADD_DISH_SUCCESS';
+export const REQUEST_ADD_DISH_FAILURE = 'DISHES::REQUEST_ADD_DISH_FAILURE';
 
 export const getDishesLoading = () => ({
-	type: REQUEST_DISHES_LOADING,
+  type: REQUEST_DISHES_LOADING,
 });
 
 export const getDishesFailure = (error) => ({
-	type: REQUEST_DISHES_FILURE,
-	payload: error,
+  type: REQUEST_DISHES_FILURE,
+  payload: error,
 });
 
 export const getDishesSuccess = (dishes) => ({
-	type: REQUEST_DISHES_SUCCESS,
-	payload: dishes,
+  type: REQUEST_DISHES_SUCCESS,
+  payload: dishes,
 });
 
 export const getDishes = () => async (dispatch) => {
-	dispatch(getDishesLoading());
+  dispatch(getDishesLoading());
 
-	try {
-		const urlFetch = 'http://213.226.114.162/api/products';
+  try {
+    const urlFetch = API + 'products';
 
-		const response = await fetch(urlFetch);
+    const response = await fetch(urlFetch);
 
-		if (!response.ok) throw new Error('Ошибка: "Dishes service not worked"');
+    if (!response.ok) throw new Error('Ошибка: "Dishes service not worked"');
 
-		const result = await response.json();
-		// console.log(result.data.length);
-		dispatch(getDishesSuccess(result.data));
-	} catch (err) {
-		dispatch(getDishesFailure(err));
-	}
+    const result = await response.json();
+    dispatch(getDishesSuccess(result.data));
+  } catch (err) {
+    dispatch(getDishesFailure(err));
+  }
+};
+
+export const addDishSuccess = (dish) => ({
+  type: REQUEST_ADD_DISH_SUCCESS,
+  payload: dish,
+});
+
+export const addDishFailure = (error) => ({
+  type: REQUEST_ADD_DISH_FAILURE,
+  payload: error,
+});
+
+export const addDishToServer = (newDish) => async (dispatch) => {
+  try {
+    const urlFetch = API + 'add-product';
+
+    const response = await fetch(urlFetch, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Request-Headers': '*',
+      },
+      body: JSON.stringify(newDish),
+    });
+
+    if (!response.ok) throw new Error('Ошибка: "Dishes service not worked"');
+
+    const result = await response.json();
+    dispatch(addDishSuccess(result));
+    return result;
+  } catch (err) {
+    dispatch(addDishFailure(err));
+  }
 };
