092664cecdd01b7ccc2d55fd1d1655231a63f8df
VeronikaDesyaterichenko <desatericenkoveronika@gmail.com>
2022-04-14 10:24:50 +0200

    updated chart calories

--- src/store/AmountNutrients/selectors.js
+++ src/store/AmountNutrients/selectors.js
@@ -1,2 +1,4 @@
 export const selectAmountNutrientsFromMeal = (mealName) => (state) => state.amountNutrients[mealName];
 export const selectAmountNutrientsFromToday = () => (state) => state.amountNutrients.totalForToday;
+export const selectAmountNutrientsNormalFromToday = () => (state) => state.amountNutrients.normalForToday;
+
