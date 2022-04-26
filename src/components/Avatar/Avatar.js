09069cb8560148f277d80e2c681a77524c66017d
VeronikaDesyaterichenko <desatericenkoveronika@gmail.com>
2022-04-26 10:25:10 +0200

    update router. Connected all the bottons of the menu with the expected link

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -1,9 +1,12 @@
+import { useNavigate } from 'react-router-dom';
 import './style.scss';
 import avatar from './woman.png';
 
 export const Avatar = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className="avatar">
+    <div onClick={() => navigate("/avatar")} className="avatar">
       <img src={avatar} alt="avatar" />
     </div>
   );
--- src/components/Routes/index.js
+++ src/components/Routes/index.js
@@ -1,19 +1,21 @@
 import { React } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Avatar } from '../Avatar/Avatar';
 import { Content } from '../Content/Content';
 import { Home } from '../Home/Home';
 import { NotFound } from '../NotFound/NotFound'
+import { Notification } from '../Notification/Notification';
 import './style.scss';
 
 export const Routers = () => {
 
-
    return (
       <BrowserRouter className="router">
-
          <Routes>
             <Route exact path="/" element={<Home />} />
             <Route exact path="/statistics" element={<Content />} />
+            {/* <Route exact path="/notification" element={<Notification />} /> */}
+            {/* <Route exact path="/avatar" element={<Avatar />}/> */}
             <Route path="*" element={<NotFound />} />
          </Routes>
       </BrowserRouter>
