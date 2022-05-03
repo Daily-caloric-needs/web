f2b657238880b83b2f89c1a6bf5567d7c7282bb2
VeronikaDesyaterichenko <desatericenkoveronika@gmail.com>
2022-05-03 18:21:55 +0200

    update calculator calories(with validation)

--- src/components/CaloriesCalculator/CaloriesCalculator.js
+++ src/components/CaloriesCalculator/CaloriesCalculator.js
@@ -11,13 +11,12 @@ import {
    TextField,
    Button,
    Typography,
-   Avatar,
+   Avatar
 } from '@mui/material';
 import { selectNormNutrients, selectNormWater } from '../../store/CaloriesCalcilator/selectors';
 import { Sidebar } from '../Sidebar/Sidebar';
 import { Search } from '../Search/Search';
 import { Notification } from '../Notification/Notification';
-
 
 export const CaloriesCalculator = () => {
 
@@ -36,6 +35,8 @@ export const CaloriesCalculator = () => {
    };
 
    const [formValues, setFormValues] = useState(defaultValues);
+   const [formValuesError, setFormValuesError] = useState(false);
+
 
    const handleInputChange = (e) => {
       if (e.target.value < 1) {
@@ -55,7 +56,7 @@ export const CaloriesCalculator = () => {
 
    const clearSubmit = () => {
       setFormValues({
-         hight : 0,
+         hight: 0,
          weight: 0,
          age: 0,
          gender: "",
@@ -72,17 +73,30 @@ export const CaloriesCalculator = () => {
       });
    }
 
+
    const calcolaNorm = (e) => {
       e.preventDefault()
+      setFormValuesError(false)
       debugger
-      if (formValues.gender === "male") {
-         formValues.calorieNorm = ((13.75 * formValues.weight) + (5 * formValues.hight) - (6.76 * formValues.age) + 66)
-         console.log(formValues.calorieNorm)
-      } else {
-         formValues.calorieNorm = ((9.56 * formValues.weight) + (1.85 * formValues.hight) - (4.68 * formValues.age) + 655)
-         console.log(formValues.calorieNorm)
+      if (formValues.weight === defaultValues.weight) {
+         setFormValuesError(true)
       }
-      calcolaFisicForm(parseInt(formValues.calorieNorm))
+      if (formValues.hight === defaultValues.hight) {
+         setFormValuesError(true)
+      }
+      if (formValues.age === defaultValues.age) {
+         setFormValuesError(true)
+      } else {
+         if (formValues.gender === "male") {
+            formValues.calorieNorm = ((13.75 * formValues.weight) + (5 * formValues.hight) - (6.76 * formValues.age) + 66)
+            console.log(formValues.calorieNorm)
+         } else {
+            formValues.calorieNorm = ((9.56 * formValues.weight) + (1.85 * formValues.hight) - (4.68 * formValues.age) + 655)
+            console.log(formValues.calorieNorm)
+         }
+         calcolaFisicForm(parseInt(formValues.calorieNorm))
+      }
+
    }
 
    const calcolaFisicForm = (norm) => {
@@ -110,7 +124,7 @@ export const CaloriesCalculator = () => {
    }
 
    return (<>
-   <div className="content">
+      <div className="content">
          <h1>Узнай свою норму</h1>
          <div className="content__header">
             <Sidebar />
@@ -122,111 +136,117 @@ export const CaloriesCalculator = () => {
             <div className="content__left">
             </div>
          </div>
-      <form onSubmit={calcolaNorm}>
-         <Grid container 
-         alignItems="center" 
-         justify="center" 
-         direction="column">
-            <Grid container justifyContent="center">
-               <TextField
-                  id="hight-input"
-                  name="hight"
-                  label="Рост"
-                  type="number"
-                  value={formValues.hight}
-                  onChange={handleInputChange}
-                  variant="filled"
-               />
-               <TextField
-                  id="weight-input"
-                  name="weight"
-                  label="Вес"
-                  type="number"
-                  value={formValues.weight}
-                  onChange={handleInputChange}
-                  variant="filled"
-               />
-               <TextField
-                  id="age-input"
-                  name="age"
-                  label="Возраст"
-                  type="number"
-                  value={formValues.age}
-                  onChange={handleInputChange}
-                  variant="filled"
-               />
+         <form noValidate autoComplete="off" onSubmit={calcolaNorm}>
+            <Grid container
+               alignItems="center"
+               justify="center"
+               direction="column">
+               <Grid container justifyContent="center">
+                  <TextField
+                     id="hight-input"
+                     name="hight"
+                     label="Рост (см)"
+                     type="number"
+                     value={formValues.hight}
+                     onChange={handleInputChange}
+                     variant="outlined"
+                     required
+                     error={formValuesError}
+                  />
+                  <TextField
+                     id="weight-input"
+                     name="weight"
+                     label="Вес (кг)"
+                     type="number"
+                     value={formValues.weight}
+                     onChange={handleInputChange}
+                     variant="outlined"
+                     required
+                     error={formValuesError}
+                  />
+                  <TextField
+                     id="age-input"
+                     name="age"
+                     label="Возраст"
+                     type="number"
+                     value={formValues.age}
+                     onChange={handleInputChange}
+                     variant="outlined"
+                     required
+                     error={formValuesError}
+                  />
+               </Grid>
+               <Grid item>
+                  <FormControl>
+                     <FormLabel>Пол</FormLabel>
+                     <RadioGroup
+                        name="gender"
+                        value={formValues.gender}
+                        onChange={handeleRadioChange}
+                        row
+                     >
+                        <FormControlLabel
+                           key="male"
+                           value="male"
+                           control={<Radio size="medium" />}
+                           label="Мужской"
+                        />
+                        <FormControlLabel
+                           key="female"
+                           value="female"
+                           control={<Radio size="medium" />}
+                           label="Женский"
+                        />
+                     </RadioGroup>
+                  </FormControl>
+               </Grid>
+               <Grid item>
+                  <FormControl>
+                     <FormLabel>Физическая активность</FormLabel>
+                     <RadioGroup
+                        name="work"
+                        value={formValues.work}
+                        onChange={handeleRadioChange}
+                        column="true"
+                     >
+                        <FormControlLabel
+                           key="weakly"
+                           value="weakly"
+                           control={<Radio size="medium" />}
+                           label="Пассивный образ жизни"
+                        />
+                        <FormControlLabel
+                           key="base"
+                           value="base"
+                           control={<Radio size="medium" />}
+                           label="Тренируюсь 1-3 раза в неделю"
+                        />
+                        <FormControlLabel
+                           key="medium"
+                           value="medium"
+                           control={<Radio size="medium" />}
+                           label="Тренируюсь 3-5 раз в неделю"
+                        />
+                        <FormControlLabel
+                           key="strong"
+                           value="strong"
+                           control={<Radio size="medium" />}
+                           label="Тренируюсь каждый день"
+                        />
+                        <FormControlLabel
+                           key="veryStrong"
+                           value="veryStrong"
+                           control={<Radio size="medium" />}
+                           label="Тренируюсь каждый день больше одного раза в день"
+                        />
+                     </RadioGroup>
+                  </FormControl>
+               </Grid>
+               <Button variant='contained' type='submit' size='medium'>Расчитать норму</Button>
             </Grid>
-            <Grid item>
-               <FormControl>
-                  <FormLabel>Пол</FormLabel>
-                  <RadioGroup
-                     name="gender"
-                     value={formValues.gender}
-                     onChange={handeleRadioChange}
-                     row
-                  >
-                     <FormControlLabel
-                        key="male"
-                        value="male"
-                        control={<Radio size="medium" />}
-                        label="Мужской"
-                     />
-                     <FormControlLabel
-                        key="female"
-                        value="female"
-                        control={<Radio size="medium" />}
-                        label="Женский"
-                     />
-                  </RadioGroup>
-               </FormControl>
-            </Grid>
-            <Grid item>
-               <FormControl>
-                  <FormLabel>Физическая активность</FormLabel>
-                  <RadioGroup
-                     name="work"
-                     value={formValues.work}
-                     onChange={handeleRadioChange}
-                     row
-                  >
-                     <FormControlLabel
-                        key="weakly"
-                        value="weakly"
-                        control={<Radio size="medium" />}
-                        label="Пассивный образ жизни"
-                     />
-                     <FormControlLabel
-                        key="base"
-                        value="base"
-                        control={<Radio size="medium" />}
-                        label="Тренируюсь 1-3 раза в неделю"
-                     />
-                     <FormControlLabel
-                        key="medium"
-                        value="medium"
-                        control={<Radio size="medium" />}
-                        label="Тренируюсь 3-5 раз в неделю"
-                     />
-                     <FormControlLabel
-                        key="strong"
-                        value="strong"
-                        control={<Radio size="medium" />}
-                        label="Тренируюсь каждый день"
-                     />
-                     <FormControlLabel
-                        key="veryStrong"
-                        value="veryStrong"
-                        control={<Radio size="medium" />}
-                        label="Тренируюсь каждый день больше одного раза в день"
-                     />
-                  </RadioGroup>
-               </FormControl>
-            </Grid>
-                  <Button disabled={formValues === defaultValues} variant='contained' type='submit' size='medium'>Расчитать норму</Button>
-         </Grid>
-      </form>
-      <Typography>Твоя суточная норма калорий: {caloriesNorm} </Typography>
-      <Typography>Твоя суточная норма воды: {normWater} </Typography>
+         </form>
+         <Typography>Твоя суточная норма калорий: {caloriesNorm} </Typography>
+         <Typography>Твоя суточная норма воды: {normWater} </Typography>
       </div>
    </>
    )
