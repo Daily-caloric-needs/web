4a94fceea56ff6ccda07e6486646bce0c8e76b9a
Aleksandra Baibik <abaibik@gmail.com>
2022-05-21 12:25:25 +0200

    improve action delete_user_data

--- src/store/UserData/actions.js
+++ src/store/UserData/actions.js
@@ -1,12 +1,11 @@
-export const ADD_USER_DATA = 'USER::ADD_USER_DATA';
-export const DELETE_USER_DATA = 'USER::DELETE_USER_DATA';
+export const ADD_USER_DATA = "USER::ADD_USER_DATA";
+export const DELETE_USER_DATA = "USER::DELETE_USER_DATA";
 
 export const addUserData = (data) => ({
   type: ADD_USER_DATA,
   payload: data,
 });
 
-export const deleteUserData = (data) => ({
+export const deleteUserData = () => ({
   type: DELETE_USER_DATA,
-  payload: data
 });
